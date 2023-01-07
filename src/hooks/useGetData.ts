a6380a4bd713a0604682b52891658564ba62605a
oleksiichornii <alexprudent777@gmail.com>
2023-01-07 17:08:06 +0200

    add fix for saving LS data

--- src/hooks/useGetData.ts
+++ src/hooks/useGetData.ts
@@ -1,30 +1,31 @@
-import { workWithAPI } from '../api/api';
-import { useEffect, useState } from 'react';
+import {workWithAPI} from '../api/api';
+import {useEffect, useState} from 'react';
 import {Data, DataType} from "../models/data";
 import {workWithLS} from "../utils/workWithLocalStorage";
 import {ARTICLE_LS_KEY} from "../utils/constants";
 import {addIdToData} from "../utils/addIdToData";
 
 export const useGetData = (): { data: DataType, loading: boolean } => {
-  const [data, setData] = useState<DataType>([]);
-  const [loading, setLoading] = useState(true);
+    const [data, setData] = useState<DataType>([]);
+    const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-      const dataFromLS = workWithLS.getData(ARTICLE_LS_KEY);
-      if(!dataFromLS) {
-          workWithAPI
-              .getData()
-              .then((res) => {
-                  setData(addIdToData(res.data as Data[]));
-                  setLoading(false);
-                  workWithLS.setData(ARTICLE_LS_KEY, res.data);
-              })
-              .catch((error) => error.log);
-      } else {
-          setData(dataFromLS as Data[]);
-          setLoading(false);
-      }
-  }, []);
+    useEffect(() => {
+        const dataFromLS = workWithLS.getData(ARTICLE_LS_KEY);
+        if (!dataFromLS) {
+            workWithAPI
+                .getData()
+                .then((res) => {
+                    const dataWithId = addIdToData(res.data as Data[]);
+                    setData(dataWithId);
+                    setLoading(false);
+                    workWithLS.setData(ARTICLE_LS_KEY, dataWithId);
+                })
+                .catch((error) => error.log);
+        } else {
+            setData(dataFromLS as Data[]);
+            setLoading(false);
+        }
+    }, []);
 
-  return { data, loading };
+    return {data, loading};
 };
