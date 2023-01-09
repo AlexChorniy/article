d874c654d206a298b476ef7e0bf7931820bf6313
oleksiichornii <alexprudent777@gmail.com>
2023-01-09 15:44:18 +0200

    Add clear LS button

--- src/components/Navigation/NavigationBar.tsx
+++ src/components/Navigation/NavigationBar.tsx
@@ -1,12 +1,21 @@
 import { Nav } from './Navigation.styled';
 import { NavigationModel, NavigationVariables } from '../../models/navigation';
 import { Button } from '../styles';
+import { workWithLS } from '../../utils/workWithLocalStorage';
+import { ARTICLE_LS_KEY, DELETED_ID_KEYS, PAGE_KEY } from '../../utils/constants';
 
 const NavigationBar = ({ onClickHandler }: { onClickHandler: NavigationVariables }): JSX.Element => {
+  const onClearHandler = () => {
+    workWithLS.removeData(PAGE_KEY);
+    workWithLS.removeData(ARTICLE_LS_KEY);
+    workWithLS.removeData(DELETED_ID_KEYS);
+  };
+
   return (
     <Nav>
       <Button onClick={() => onClickHandler(NavigationModel.previous)}>&lt; Previous</Button>
       <Button onClick={() => onClickHandler(NavigationModel.next)}>Next &gt;</Button>
+      <Button onClick={onClearHandler}>Clear LS</Button>
     </Nav>
   );
 };
